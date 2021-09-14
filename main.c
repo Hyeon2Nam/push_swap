c83d976ee4525907bcfade68e5c9d3144f8318c1
hyenam <hyenam@student.42seoul.kr>
2021-09-14 14:37:28 +0900

    first

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hyenam <hyeon@student.42seoul.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 14:16:11 by hyenam            #+#    #+#             */
-/*   Updated: 2021/09/14 13:50:29 by hyenam           ###   ########.fr       */
+/*   Updated: 2021/09/14 14:34:16 by hyenam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 void	error_handler(void)
 {
 	ft_putstr_fd("Error\n");
-	// int n;
-	// putchar(scanf("%d",&n));
 	exit(1);
 }
 
@@ -51,35 +49,6 @@ void	input_data(t_stack *stack, int argc, char *args[])
 		exit(0);
 }
 
-void find_error(t_stack *stack)
-{
-	t_node	*cur;
-	int		*arr;
-	int		i;
-
-	i = -1;
-	cur = stack->head;
-	arr = (int *)malloc(sizeof(int) * (stack->size + 1));
-	if (!arr)
-		return ;
-	while (++i <= stack->size)
-	{
-		arr[i] = cur->data;
-		cur = cur->next;
-	}
-	quick_sort(arr, 0, stack->size - 1);
-	cur = stack->head;
-	printf("adsf");
-	for (int j = 0; j < stack->size; j++)
-	{
-		if (arr[j] != cur->data)
-			printf("%d번째 데이터 오류 quick:%d stack:%d\n", j, arr[j], cur->data);
-		cur = cur->next;
-	}
-	
-	free(arr);
-}
-
 int	main(int argc, char *args[])
 {
 	t_stack	*stack;
@@ -89,10 +58,6 @@ int	main(int argc, char *args[])
 	stack = init_stack();
 	input_data(stack, argc, args);
 	sort_case(stack);
-	// print_stack(stack);
-	// find_error(stack);
 	reset_stack(stack);
 	free(stack);
-	// int n;
-	// putchar(scanf("%d",&n));
 }
